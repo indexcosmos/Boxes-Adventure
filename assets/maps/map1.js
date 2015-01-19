4a6269329504507dfc270ee86664985b349f5b34
badpeaches <badpeaches@gmail.com>
2015-01-18 20:34:15 -0500

    add files

---
+++ assets/maps/map1.js
@@ -0,0 +1,74 @@
+var boxes = [{
+  x: 0,
+  y: 0,
+  width: 10,
+  height: canvas.height
+},
+{
+  x: 0,
+  y: height - 2,
+  width: canvas.width,
+  height: 50
+},
+{
+  x: width - 10,
+  y: 0,
+  width: 50,
+  height: canvas.height
+},
+
+{
+  x: 120,
+  y: 10,
+  width: 80,
+  height: 80
+},
+{
+  x: 170,
+  y: 50,
+  width: 80,
+  height: 80
+},
+{
+  x: 220,
+  y: 100,
+  width: 80,
+  height: 80
+},
+{
+  x: 270,
+  y: 150,
+  width: 80,
+  height: 80
+},
+{
+  x: 320,
+  y: 200,
+  width: 80,
+  height: 80
+},
+{
+  x: 370,
+  y: 250,
+  width: 80,
+  height: 80
+},
+{
+  x: 420,
+  y: 300,
+  width: 80,
+  height: 80
+},
+{
+  x: 470,
+  y: 350,
+  width: 40,
+  height: 40
+},
+{
+  x: 520,
+  y: 400,
+  width: 80,
+  height: 80
+}
+];
