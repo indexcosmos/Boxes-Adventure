225b860b78e19d540e4a01874294edfa12ceb322
badpeaches <badpeaches@gmail.com>
2015-01-29 23:32:05 -0500

    map 2

---
+++ assets/maps/js/2.js
@@ -0,0 +1,94 @@
+
+
+var map = {
+
+  width: 900,
+  height: 450,
+  friction: 0.8,
+  gravity: 0.3,
+
+  player: {
+
+    x: 500,
+    y: 350,
+    width: 38,
+    height: 38,
+    speed: 3,
+    dir: "l",
+    start_profile: "box",
+    finish_profile: "box"
+
+  },
+
+  boxes: [
+
+    // floor
+    {
+      x: 0,
+      y: 450 - 55,
+      width: 900,
+      height: 55
+    },
+
+    // first level left wall
+    {
+      x: 7,
+      y: 282,
+      width: 740,
+      height: 50
+    },
+    // second level right wall
+    {
+      x: 158,
+      y: 173,
+      width: 740,
+      height: 50
+    },
+    // third level left wall
+    {
+      x: 7,
+      y: 65,
+      width: 740,
+      height: 50
+    },
+
+
+  ],
+
+  rewards: [
+
+    // circle
+    {
+      x: 669,
+      y: 34,
+      width: 10,
+      height: 10,
+      profile: "circle"
+    },
+    // star
+    {
+      x: 173,
+      y: 139,
+      width: 10,
+      height: 10,
+      profile: "star"
+    },
+    // box
+    {
+      x: 105,
+      y: 360,
+      width: 10,
+      height: 10,
+      profile: "box"
+    },
+
+  ],
+
+  hole: {
+    x: 22,
+    y: 345,
+    width: 40,
+    height: 40
+  }
+
+};
